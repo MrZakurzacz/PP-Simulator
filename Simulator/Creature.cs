b25048454530905fbd179b75d4a5a8613ae25980
Jakub Dziektarz <MrZakurzacz@gmail.com>
2024-10-29 22:46:50 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature()
+    {
+    }
+    public string Info => $"{Name} (Level {Level})";
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi! I'm {Name} at Level {Level}.");
+    }
+}
