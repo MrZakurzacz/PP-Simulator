3afc19f4c0f2b43868c6365b286bb65651542e52
Jakub Dziektarz <MrZakurzacz@gmail.com>
2024-10-29 22:51:30 +0100

    Settery

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -8,21 +8,76 @@ namespace Simulator;
 
 public class Creature
 {
-    public string Name { get; set; }
-    public int Level { get; set; }
-    public Creature(string name, int level = 1)
+    private string _name = "Unknown";
+    private int _level = 1;
+    private bool _nameSet = false;
+    private bool _levelSet = false;
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (_nameSet) return;
+
+            value = value.Trim();
+
+            if (value.Length < 3)
+            {
+                value = value.PadRight(3, '#');
+            }
+
+            if (value.Length > 25)
+            {
+                value = value.Substring(0, 25).TrimEnd();
+                if (value.Length < 3)
+                {
+                    value = value.PadRight(3, '#');
+                }
+            }
+
+            if (char.IsLower(value[0]))
+            {
+                value = char.ToUpper(value[0]) + value.Substring(1);
+            }
+
+            _name = value;
+            _nameSet = true;
+        }
+    }
+
+    public int Level
+    {
+        get => _level;
+        set
+        {
+            if (_levelSet) return;
+
+            _level = Math.Clamp(value, 1, 10);
+            _levelSet = true;
+        }
+    }
+
+    public Creature(string name = "Unknown", int level = 1)
     {
         Name = name;
         Level = level;
     }
 
-    public Creature()
-    {
-    }
+    public Creature() { }
+
     public string Info => $"{Name} (Level {Level})";
 
     public void SayHi()
     {
         Console.WriteLine($"Hi! I'm {Name} at Level {Level}.");
+    }
+
+    public void Upgrade()
+    {
+        if (Level < 10)
+        {
+            Level++;
+        }
     }
 }
